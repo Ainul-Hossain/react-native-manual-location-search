78bbc4ad2ed1a6a7238ceda13d1822f23a89a21a
Ainul-Hossain <ainulsakibs@gmail.com>
2023-11-04 15:49:26 +0600

    Smooth Search Implementation Added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { FlatList, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { ActivityIndicator, FlatList, Platform, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const NOMINATIM_BASE_URL = `https://nominatim.openstreetmap.org/search?`;
@@ -11,12 +11,37 @@ export default function App() {
 
   const [locationInfo, setLocationInfo] = useState([]);
 
-  const handleLocationSearch = () => {
+  const [abortController, setAbortController] = useState(new AbortController());
+
+  const [loading, setLoading] = useState(false);
+
+  const [typingTimeout, setTypingTimeout] = useState(0);
+
+  useEffect(() => {
+    return () => {
+      // Cleanup the AbortController when the component unmounts
+      abortController.abort();
+    };
+  }, [abortController]);
+
+  const handleLocationSearch = (text) => {
+    // console.log(text);
+    setLoading(true);
+
+    abortController.abort();
+
+    const newAbortController = new AbortController();
+    setAbortController(newAbortController);
+
+    // Clear the previous typingTimeout if it exists
+    clearTimeout(typingTimeout);
+
     const params = {
-      q: searchString,
+      q: typeof text !== 'string' ? searchString : text,
       format: 'json',
       addressdetails: 1,
-      limit: 40
+      limit: 40,
+      'accept-language': 'en-US,en;q=0.5'
     }
 
     const requestedOptions = {
@@ -26,23 +51,47 @@ export default function App() {
 
     const queryString = new URLSearchParams(params).toString();
 
-    fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestedOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        let x = data.map((val, i) => {
-          return {
-            osm_id: val.osm_id,
-            display_name: val.display_name,
-            city: val.address?.city,
-            lat: val.lat,
-            lon: val.lon
-          };
+    // fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestedOptions)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // console.log(data);
+    //     let x = data.map((val, i) => {
+    //       return {
+    //         osm_id: val.osm_id,
+    //         display_name: val.display_name,
+    //         city: val.address?.city,
+    //         lat: val.lat,
+    //         lon: val.lon
+    //       };
+    //     })
+    //     console.log(x);
+    //     setLocationInfo(x);
+    //   })
+    //   .catch((err) => console.log(err.message));
+
+    const newTypingTimeout = setTimeout(() => {
+      // Make the API request
+      fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestedOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data);
+          let x = data.map((val, i) => {
+            return {
+              osm_id: val.osm_id,
+              display_name: val.display_name,
+              city: val.address?.city,
+              lat: val.lat,
+              lon: val.lon
+            };
+          })
+          console.log(x);
+          setLocationInfo(x);
         })
-        console.log(x);
-        setLocationInfo(x);
-      })
-      .catch((err) => console.log(err.message));
+        .catch((err) => console.log(err.message))
+        .finally(() => setLoading(false));
+    }, 400); // Adjust the debounce time as needed
+
+    setTypingTimeout(newTypingTimeout);
   }
 
   const Item = ({ osm_id, display_name, lat, lon }) => (
@@ -50,7 +99,7 @@ export default function App() {
       android_ripple={{
         color: 'gray'
       }}
-      onPress={() => console.log(lat, lon)}
+      onPress={() => console.log(lat, lon, display_name.split(',')[0])}
       style={{
         padding: 10
       }}
@@ -70,7 +119,7 @@ export default function App() {
   );
 
   return (
-    <SafeAreaView style={{ flex: 1, paddingTop: '50%' }}>
+    <SafeAreaView style={{ flex: 1, paddingTop: Platform.OS !== 'ios' ? 30 : null }}>
       <StatusBar style="auto" />
 
       <View style={styles.container}>
@@ -78,6 +127,7 @@ export default function App() {
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <TextInput
             style={{
+              width: 250,
               height: 40,
               padding: 10,
               borderColor: 'gray',
@@ -86,7 +136,7 @@ export default function App() {
             }}
             onChangeText={text => {
               setSearchString(text);
-              // handleLocationSearch();
+              handleLocationSearch(text);
             }}
             onSubmitEditing={handleLocationSearch}
             value={searchString}
@@ -102,6 +152,8 @@ export default function App() {
             onPress={handleLocationSearch}
           />
         </View>
+
+        {loading ? <ActivityIndicator style={{paddingTop: 10}} size="small" color="#0000ff" /> : null}
 
         <FlatList
           data={locationInfo}
